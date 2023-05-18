b72989473ec2fcceebd1a8f212304fe9d0e4f182
Maurice Nyangede <mnyangede@gmail.com>
2023-05-18 09:33:08 +0300

    Practise Code

---
+++ Hello.py
@@ -0,0 +1,29 @@
+# Clear the terminal screen
+import os
+os.system('clear')
+
+#################################
+# DATA TYPES
+# Strings 
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+#################################
+
+
+first_name = "Elder"
+age = 23
+names = ["John", "Bob", "Mary"]
+names = ("John", "Bob", "Mary")
+fav_pizza = {
+	"John":"Pepperoni",
+	"Mary": "Mushroom",
+	"Bob": "Cheese"
+}
+name = True 
+
+print (name) 
+
+
